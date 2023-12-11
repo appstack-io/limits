84383d522815c26758c30f746c5276a2ecf498be
orgal <or.gal@vee.com>
2023-12-11 15:50:01 +0200

    fix: init

--- src/limits.module.ts
+++ src/limits.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { RpcRateLimitReadInterceptor } from './rpcRateLimitRead.interceptor';
 import { RpcRateLimitWriteInterceptor } from './rpcRateLimitWrite.interceptor';
 import { Utils } from './utils';
+import { RedisModule } from '@appstack-io/redis';
 
 @Module({
+  imports: [RedisModule],
   providers: [RpcRateLimitReadInterceptor, RpcRateLimitWriteInterceptor, Utils],
   exports: [RpcRateLimitReadInterceptor, RpcRateLimitWriteInterceptor],
 })
